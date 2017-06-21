22c7c71013894e478d5061bd9f77ca8e79cfc8e4
Zeno Rocha <zno.rocha@gmail.com>
2017-06-21 15:47:21 -0700

    Creates user on the database before starting the game

--- web/assets/scripts/home.js
+++ web/assets/scripts/home.js
@@ -1,4 +1,4 @@
-var auth = WeDeploy.auth('auth-lifebringer.wedeploy.xyz');
+var auth = WeDeploy.auth('auth-ccc.liferay.com');
 var currentUser = WeDeploy.auth().currentUser;
 
 if (currentUser) {
@@ -17,6 +17,41 @@ btn.addEventListener('click', function(e) {
 
 // Redirect
 
-auth.onSignIn(function(user) {
-	document.location.href = '/game/';
+auth.onSignIn(function(currentUser) {
+	currentUser.id = window.md5(currentUser.email);
+
+	WeDeploy
+		.data('db-ccc.liferay.com')
+		.where('id', currentUser.id)
+		.get('game')
+		.then(function(user) {
+			if (user.length > 0) {
+				document.location.href = '/game/';
+			} else {
+				createUser(currentUser);
+			}
+		})
+		.catch(function() {
+			alert('Something wrong happened, try later.');
+		})
 });
+
+function createUser(currentUser) {
+	WeDeploy
+		.data('db-ccc.liferay.com')
+		.create('game', {
+			id: currentUser.id,
+			name: currentUser.name,
+			email: currentUser.email,
+			photoUrl: currentUser.photoUrl,
+			count: 0,
+			maxScore: 0,
+			games: []
+		})
+		.then(function() {
+			document.location.href = '/game/';
+		})
+		.catch(function() {
+			alert('Something wrong happened, try later.');
+		})
+}
